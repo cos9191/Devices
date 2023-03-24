08b19448749133f3d4be61ca2ef2382a2d02dc1f
Konstantin Fedorov <cos9191@mail.ru>
2023-03-24 22:27:02 +0700

    ПП, исправил ошибку JS при отсутствии блока со спойлером, исправил шрифт моно (был без русских символов),

--- source/js/modules/accordion/spoiler.js
+++ source/js/modules/accordion/spoiler.js
@@ -1,17 +1,42 @@
 const spoiler = document.querySelector('[data-spoiler="parent"]');
-const spoilerButton = spoiler.querySelector('[data-accordion="button"]');
 
-const spoilerBtnToggler = () => {
-  if (!spoiler.classList.contains('is-active')) {
-    spoilerButton.textContent = 'СВЕРНУТЬ';
-    return;
-  }
-  spoilerButton.textContent = 'ПОДРОБНЕЕ';
-};
-const initSpoiler = () => {
-  if (spoiler && spoilerButton) {
-    spoilerButton.addEventListener('click', spoilerBtnToggler);
-  }
-};
+let spoilerButton;
+if (spoiler) {
+  spoilerButton = spoiler.querySelector('[data-accordion="button"]');
+}
+
+let initSpoiler = () => {};
+
+if (spoilerButton) {
+  const spoilerBtnToggler = () => {
+    if (spoiler && spoilerButton && !spoiler.classList.contains('is-active')) {
+      spoilerButton.textContent = 'СВЕРНУТЬ';
+      return;
+    }
+    spoilerButton.textContent = 'ПОДРОБНЕЕ';
+  };
+  initSpoiler = () => {
+    if (spoiler && spoilerButton) {
+      spoilerButton.addEventListener('click', spoilerBtnToggler);
+    }
+  };
+
+  initSpoiler();
+}
 
 export {initSpoiler};
+
+// const spoilerBtnToggler = () => {
+//   if (spoiler && spoilerButton && !spoiler.classList.contains('is-active')) {
+//     spoilerButton.textContent = 'СВЕРНУТЬ';
+//     return;
+//   }
+//   spoilerButton.textContent = 'ПОДРОБНЕЕ';
+// };
+// const initSpoiler = () => {
+//   if (spoiler && spoilerButton) {
+//     spoilerButton.addEventListener('click', spoilerBtnToggler);
+//   }
+// };
+//
+// export {initSpoiler};
