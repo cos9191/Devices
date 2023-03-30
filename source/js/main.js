04117fbf23f797fef2068ff3dcae06fc923e60d1
Konstantin Fedorov <cos9191@mail.ru>
2023-03-30 23:26:53 +0700

    fix modal autofocus styled sitemap deleted unused vendor js

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,5 @@
 import {iosVhFix} from './utils/ios-vh-fix';
+import {} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 import {initAccordions} from './modules/accordion/init-accordion';
@@ -16,13 +17,16 @@ window.addEventListener('DOMContentLoaded', () => {
   // Modules
   // ---------------------------------
 
+
+
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    initModals();
+
     const form = new Form();
     window.form = form;
     form.init();
+    initModals();
     initAccordions();
     initSpoiler();
   });
